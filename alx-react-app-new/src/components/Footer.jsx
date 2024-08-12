c9f5ea09fb61deb37acec924631cf8c81d3a62ee
Kings <kingsley.okechukwu@outlook.com>
2024-08-12 04:06:24 +0100

    New react app added

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer style={{fontFamily: 'Candara',font: 'bold 40px'}}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
