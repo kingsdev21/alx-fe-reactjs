298bd1110418f3f846455f1e5bae3a40e9c9e17d
Kings <kingsley.okechukwu@outlook.com>
2024-08-18 23:26:17 +0100

    Task 01

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer.jsx'
 import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
   
@@ -16,8 +17,8 @@ function App() {
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />    
-
     </>
   )
 }
