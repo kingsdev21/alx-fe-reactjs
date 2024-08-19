bb2606f431e2293352438a0b371351f0bc73284f
Kings <kingsley.okechukwu@outlook.com>
2024-08-19 06:03:58 +0100

    Task 3

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+// import WelcomeMessage from './components/WelcomeMessage'
+// import Header from './components/Header'
+// import MainContent from './components/MainContent'
+// import Footer from './components/Footer.jsx'
+// import UserProfile from './components/UserProfile.jsx'
+import React from 'react';
+import ProfilePage from './components/ProfilePage';
+import userContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };  
+
+  return(
+  <userContext.Provider value={userData}>
+   <ProfilePage />
+  </userContext.Provider>
+  // <ProfilePage />
+  );
+}
+
+export default App
